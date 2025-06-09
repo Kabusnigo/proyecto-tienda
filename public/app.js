06a0063f19ea75f70cdabf108e71f458cde1bb7b
Mateo J. Sosa <mateososa134@hotmail.com>
2025-06-09 11:56:58 -0300

    fix it ubicacion del service worker

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('public/service-worker.js')
+        navigator.serviceWorker.register('/service-worker.js')
         .then(function() {
             console.log('ServiceWorker registered!');
         }).catch(function (err){
