ab98e51ac771f6dff30aae457f00949259fdcb50
Mateo J. Sosa <mateososa134@hotmail.com>
2025-06-09 11:48:41 -0300

    app.js creado

--- index.js
+++ index.js
@@ -50,17 +50,7 @@ app.listen(puerto, () => {
 
 //  -------------- END OF OPEN SERVER IN PORT --------------
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('public/service-worker.js')
-        .then(function() {
-            console.log('ServiceWorker registered!');
-        }).catch(function (err){
-            console.log('ServiceWorker failed',err);
-        });
-    })
-  
-}
+
 
 
 
