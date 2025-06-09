dfe22edc9fc6f5edc64c446a131a3756ff5b2d74
Mateo J. Sosa <mateososa134@hotmail.com>
2025-06-09 12:07:18 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,22 @@
-self.addEventListener('install', () => {
-    console.log('SW instalado');
+const cacheName = 'fast-cache';
 
-    
+const filesToCache = [
+    '/index.html',
+    '/css/style.css',
+    '/img/logo.png',
+    '/img/hombres.webp',
+    '/img/mujeres.webp',
+    '/img/header-02.png',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js',
+    'https://fonts.googleapis.com/css2?family=Roboto+Condensed'
+]
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(function (cache){
+            console.log('[ServiceWoker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
 });
